3c56b921efb2ffe5f1b5543bd3145655a85e64a0
Dongha Choi <carmania0701@gmail.com>
2024-05-27 20:08:47 +0900

    TailwindCSS Download

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#E2EE83',
+      },
+    },
+  },
+  plugins: [],
+};
+
+module.exports = {
+  content: ['./src/**/*.{js,jsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
